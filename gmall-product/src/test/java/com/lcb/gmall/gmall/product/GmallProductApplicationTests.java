72955b56326d76a62ecfc5c203f7cf1c66c47fb2
lcb <2990024235@qq.com>
2022-03-19 21:55:02 +0800

    项目初始结构创建

---
+++ gmall-product/src/test/java/com/lcb/gmall/gmall/product/GmallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcb.gmall.gmall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

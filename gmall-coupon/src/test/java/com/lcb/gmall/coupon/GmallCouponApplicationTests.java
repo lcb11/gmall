0280bc8819ca8287319f07ff88556bc3c31cde86
lcb <2990024235@qq.com>
2022-03-19 21:54:52 +0800

    项目初始结构创建

---
+++ gmall-coupon/src/test/java/com/lcb/gmall/coupon/GmallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcb.gmall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gmall-ware/src/test/java/com/lcb/gmall/ware/GmallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcb.gmall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
